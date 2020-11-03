498f9bec4684fd8099fa22c66aa999648db94517
EngSuhail <73076012+EngSuhail@users.noreply.github.com>
2020-11-03 12:53:47 +0200

    Second Apps Square

---
+++ app/src/main/java/com/example/android/finalconstraintfinal/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android.finalconstraintfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btClick: Button
+    lateinit var etName: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btClick = findViewById<Button>(R.id.btt)
+        etName = findViewById<EditText>(R.id.txt)
+
+    }
+    fun display(view: View) {
+        var name: String = etName.getText().toString().trim()
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
+    }
+    }
